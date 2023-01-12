c31946aee92b1cfde555851c827a2e22f1b75257
Ravi Sisodia <ravi.sisodia@ens.enterprises>
2023-01-12 12:30:09 +0530

    headernannouncement

---
+++ assets/accordion.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  document.getElementById("change-content").innerHTML = "Paragraph changed.";
+}
+
+
+ document.getElementsByTagName("a")[0].addEventListener("click",function(){
+    document.getElementsByClassName("content")[0].style.display="block";
+        
+  });
+
+
+
